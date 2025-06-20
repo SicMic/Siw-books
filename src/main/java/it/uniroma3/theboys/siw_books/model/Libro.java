a94856aa0be62b4c7f5cd55105772657d0581b23
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-06-20 11:52:22 +0200

    seconda commit

--- src/main/java/it/uniroma3/theboys/siw_books/model/Libro.java
+++ src/main/java/it/uniroma3/theboys/siw_books/model/Libro.java
@@ -21,19 +21,8 @@ abstract public class Libro {
     private Long id;
 
     @Column(nullable = false)
-    private String nome;
+    private String titolo;
 
-    @Column(nullable = false)
-    private String cognome;
-
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    @Column(length = 500)
-    private String urlImage;
-
-    @OneToOne
-    private Credenziali credenziali;
 
     public Long getId() {
         return id;
@@ -41,72 +30,6 @@ abstract public class Libro {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Credenziali getCredenziali() {
-        return credenziali;
-    }
-
-    public void setCredenziali(Credenziali credenziali) {
-        this.credenziali = credenziali;
-    }
-
-    public String getUrlImage() {
-        return urlImage;
-    }
-
-    public void setUrlImage(String urlImage) {
-        this.urlImage = urlImage;
-    }
-
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Utente other = (Utente) obj;
-        if (email == null) {
-            if (other.email != null)
-                return false;
-        } else if (!email.equals(other.email))
-            return false;
-        return true;
     }
 
 }
