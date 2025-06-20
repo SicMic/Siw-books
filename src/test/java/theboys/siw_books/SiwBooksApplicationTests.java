723a20741de63fd20059016e456d4746ac055e91
Michela Sicuranza <michela.sicuranza@gmail.com>
2025-06-20 11:13:15 +0200

    project initializer

---
+++ src/test/java/theboys/siw_books/SiwBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package theboys.siw_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
